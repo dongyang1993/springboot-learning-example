3fb8a671ce6ade47a3311a52d72e72a8fe601971
Henry <dongyang1993@outlook.com>
2020-11-05 22:12:18 +0800

    通过数据库认证用户名密码模块

---
+++ springboot-security/springboot-security-learn02/src/main/java/security/SecurityLearn02Application.java
@@ -0,0 +1,11 @@
+package security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLearn02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLearn02Application.class, args);
+    }
+}
