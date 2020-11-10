58071ed01a67931002c44ee0c140518e57da7985
Henry <dongyang1993@outlook.com>
2020-11-10 15:26:03 +0800

    simple

--- springboot-security/springboot-security-learn03/src/main/resources/init.sql
+++ springboot-security/springboot-security-learn03/src/main/resources/init.sql
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `t_user`;
 
 CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(20) NOT NULL COMMENT '用户名',
+  `username` varchar(20) NOT NULL COMMENT '用户名',
   `password` varchar(20) NOT NULL COMMENT '密码',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
