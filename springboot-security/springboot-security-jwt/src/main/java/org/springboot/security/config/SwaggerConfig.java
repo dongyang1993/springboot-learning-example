1de9f1d65d73da328de19352914667142accd375
Henry <dongyang1993@outlook.com>
2020-12-24 10:38:45 +0800

    swagger

---
+++ springboot-security/springboot-security-jwt/src/main/java/org/springboot/security/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package org.springboot.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
+@Configuration
+public class SwaggerConfig {
+}
