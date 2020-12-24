73cc76c5490a1013526b8ebc2dc7082368f76bfd
Henry <dongyang1993@outlook.com>
2020-12-24 11:22:16 +0800

    swagger

--- springboot-security/springboot-security-jwt/src/main/java/org/springboot/security/controller/MineUserController.java
+++ springboot-security/springboot-security-jwt/src/main/java/org/springboot/security/controller/MineUserController.java
@@ -1,6 +1,7 @@
 package org.springboot.security.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springboot.security.common.api.Rs;
 import org.springboot.security.entity.MineUser;
 import org.springboot.security.service.MineUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,8 @@ public class MineUserController {
 
     @ApiOperation(value = "获取全部用户列表")
     @GetMapping("/listAll")
-    public List<MineUser> listAll() {
-        return mineUserService.listAll();
+    public Rs<List<MineUser>> listAll() {
+        List<MineUser> userList = mineUserService.listAll();
+        return Rs.success(userList);
     }
 }
