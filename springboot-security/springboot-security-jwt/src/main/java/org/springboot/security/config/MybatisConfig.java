9ddd0bc81d3fae8a6f7da8781855039880057fa5
Henry <dongyang1993@outlook.com>
2021-01-13 11:12:04 +0800

    jwt

--- springboot-security/springboot-security-jwt/src/main/java/org/springboot/security/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package org.springboot.security.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("org.springboot.security.dao")
-public class MybatisConfig {
-}
--- springboot-security/springboot-security-jwt/src/main/java/org/springboot/security/dao/MineUserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.springboot.security.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.springboot.security.entity.MineUser;
-
-@Mapper
-public interface MineUserMapper extends BaseMapper<MineUser> {
-}
