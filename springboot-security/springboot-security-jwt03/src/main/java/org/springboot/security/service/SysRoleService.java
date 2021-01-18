60219c7a42dde57c2e27eb2e42ed34944a92a5c1
Henry <dongyang1993@outlook.com>
2021-01-18 20:48:50 +0800

    jwt

--- springboot-security/springboot-security-jwt03/src/main/java/org/springboot/security/service/SysRoleService.java
+++ springboot-security/springboot-security-jwt03/src/main/java/org/springboot/security/service/SysRoleService.java
@@ -1,4 +1,16 @@
 package org.springboot.security.service;
 
+import org.springboot.security.entity.SysRole;
+
+import java.util.List;
+
 public interface SysRoleService {
+
+    void save(SysRole sysRole);
+
+    void deleteById(String id);
+
+    SysRole getById(String id);
+
+    List<SysRole> listAll();
 }
--- springboot-security/springboot-security-jwt03/src/main/java/org/springboot/security/service/impl/SysRoleServiceImpl.java
+++ springboot-security/springboot-security-jwt03/src/main/java/org/springboot/security/service/impl/SysRoleServiceImpl.java
@@ -1,10 +1,40 @@
 package org.springboot.security.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springboot.security.dao.SysRoleMapper;
+import org.springboot.security.entity.SysRole;
 import org.springboot.security.service.SysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class SysRoleServiceImpl implements SysRoleService {
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
+    @Override
+    public void save(SysRole sysRole) {
+        sysRoleMapper.insert(sysRole);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        sysRoleMapper.deleteById(id);
+    }
+
+    @Override
+    public SysRole getById(String id) {
+        return sysRoleMapper.selectById(id);
+    }
+
+    @Override
+    public List<SysRole> listAll() {
+        QueryWrapper<SysRole> wrapper = new QueryWrapper<>();
+        return sysRoleMapper.selectList(wrapper);
+    }
 }
